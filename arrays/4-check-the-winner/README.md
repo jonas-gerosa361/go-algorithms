c6c21b5443c1d49a0abb7e51825a28ccbad3b5f9
jonas-gerosa361 <ti.jonas361@gmail.com>
2022-10-21 15:12:52 -0300

    Fixing the exercise 4

---
+++ arrays/4-check-the-winner/README.md
@@ -0,0 +1,12 @@
+## QUESTION:
+Given a array of competitions as: [
+  ["HTML", "C#"], // C# won
+  ["Python", "C#"], // C# won
+  ["Python", "HTML"] // Python won
+]
+
+And a array of results - considering 0 as the left team is the winner
+[0, 0, 1]
+
+Check who won the final competition in this example:
+C# won with 6 points followed by Python with 3 points
---
+++ arrays/4-check-the-winner/solution.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func TournamentWinner(competitions [][]string, results []int) string {
+	var winner string
+	teams := make(map[string]int)
+
+	for index, result := range results {
+		if result == 0 {
+			result = 1
+		} else if result == 1 {
+			result = 0
+		}
+		teams[competitions[index][result]] += 3
+	}
+
+	var maxPoints int
+	for team, points := range teams {
+		if points > maxPoints {
+			maxPoints = points
+		}
+
+		if points <= maxPoints {
+			winner = team
+		}
+	}
+
+	return winner
+}
+
+func main() {
+	competitions := [][]string{
+		{"HTML", "Java"},
+		{"Java", "Python"},
+		{"Python", "HTML"},
+		{"C#", "Python"},
+		{"Java", "C#"},
+		{"C#", "HTML"},
+		{"SQL", "C#"},
+		{"HTML", "SQL"},
+		{"SQL", "Python"},
+		{"SQL", "Java"},
+	}
+
+	results := []int{0, 1, 1, 1, 0, 1, 0, 1, 1, 0}
+
+	winner := TournamentWinner(competitions, results)
+	fmt.Println(winner)
+}
